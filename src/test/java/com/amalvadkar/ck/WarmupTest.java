fec8e6537bdc1479bd18c0619d8cabcf5a1bb738
Abhishek <abhi@dev.com>
2025-11-01 20:49:52 +0530

    Created warmup test

---
+++ src/test/java/com/amalvadkar/ck/WarmupTest.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.ck;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WarmupTest {
+
+    @Test
+    void should_pass() {
+        assertThat(true).isTrue();
+    }
+}
