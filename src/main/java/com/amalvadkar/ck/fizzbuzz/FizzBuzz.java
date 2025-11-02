34cf73a078c922d630a4e5a2920d0cff27356cbe
Abhishek <abhi@dev.com>
2025-11-02 19:06:42 +0530

    Final solution

--- src/main/java/com/amalvadkar/ck/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/amalvadkar/ck/fizzbuzz/FizzBuzz.java
@@ -9,14 +9,66 @@ public class FizzBuzz {
     public static final String FIZZ_BUZZ = FIZZ + BUZZ;
     public static final int NUMBER_FIVE = 5;
     public static final int NUMBER_THREE = 3;
+    public static final String EMPTY_STRING = "";
+    public static final char CHARACTER_FIVE = '5';
+    public static final char CHARACTER_THREE = '3';
 
     public String speak(String number) {
+        if (hasSingleCharacter(number) && isDivisibleByThreeOrFive(number)) {
+            return prepareResultForDivisibleByThreeOrFiveOrBothCase(number);
+        }
+        String prepareResultForNumberHasFiveOrThreeAlongWithDivisibleCase = prepareResultForNumberHasFiveOrThreeAlongWithDivisibleCase(number);
+        if (hasResult(prepareResultForNumberHasFiveOrThreeAlongWithDivisibleCase)) {
+            return prepareResultForNumberHasFiveOrThreeAlongWithDivisibleCase;
+        }
+        return number;
+    }
 
-        if(isDivisibleByThreeAndFiveBoth(number)) return FIZZ_BUZZ;
+    private static boolean hasResult(String prepareResultForNumberHasFiveOrThreeAlongWithDivisibleCase) {
+        return !prepareResultForNumberHasFiveOrThreeAlongWithDivisibleCase.isEmpty();
+    }
+
+    private static boolean isDivisibleByThreeOrFive(String number) {
+        return isDivisibleByThree(number) || isDivisibleByFive(number);
+    }
+
+    private static boolean hasSingleCharacter(String number) {
+        return number.length() == 1;
+    }
+
+    private static String prepareResultForNumberHasFiveOrThreeAlongWithDivisibleCase(String number) {
+        String resultForNumberHasThreeOrFiveInIt = hasFiveOrThreeInIt(number);
+        String resultFromDivisibleByThreeOrFive = prepareResultForDivisibleByThreeOrFiveOrBothCase(number);
+        return resultForNumberHasThreeOrFiveInIt.concat(resultFromDivisibleByThreeOrFive);
+    }
+
+    private static String prepareResultForDivisibleByThreeOrFiveOrBothCase(String number) {
+        if (isDivisibleByThreeAndFiveBoth(number)) return FIZZ_BUZZ;
         if (isDivisibleByThree(number)) return FIZZ;
         if (isDivisibleByFive(number)) return BUZZ;
+        return EMPTY_STRING;
+    }
 
-        return number;
+    private static String hasFiveOrThreeInIt(String number) {
+        StringBuilder hasFiveOrThreeResultBuilder = new StringBuilder();
+        for (int i = 0; i < number.length(); i++) {
+            char currentCharacter = number.charAt(i);
+            if (isThree(currentCharacter)) {
+                hasFiveOrThreeResultBuilder.append(FIZZ);
+            }
+            if (isFive(currentCharacter)) {
+                hasFiveOrThreeResultBuilder.append(BUZZ);
+            }
+        }
+        return hasFiveOrThreeResultBuilder.toString();
+    }
+
+    private static boolean isFive(char currentCharacter) {
+        return currentCharacter == CHARACTER_FIVE;
+    }
+
+    private static boolean isThree(char currentCharacter) {
+        return currentCharacter == CHARACTER_THREE;
     }
 
     private static boolean isDivisibleByThreeAndFiveBoth(String number) {
--- src/test/java/com/amalvadkar/ck/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/amalvadkar/ck/fizzbuzz/FizzBuzzTest.java
@@ -34,6 +34,30 @@ public class FizzBuzzTest {
     @Test
     void should_return_FizzBuzz_if_input_is_divisible_by_3_and_5_both() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz.speak("15")).isEqualTo("FizzBuzz");
+        assertThat(fizzBuzz.speak("15")).isEqualTo("BuzzFizzBuzz");
+    }
+
+    @Test
+    void should_return_BuzzFizz_if_input_is_5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.speak("53")).isEqualTo("BuzzFizz");
+    }
+
+    @Test
+    void should_return_Fizz_if_input_has_contains_two_time_5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.speak("559")).isEqualTo("BuzzBuzz");
+    }
+
+    @Test
+    void should_return_Fizz_if_input_has_contains_5_and_divisible_by_5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.speak("55")).isEqualTo("BuzzBuzzBuzz");
+    }
+
+    @Test
+    void should_return_FizzBuzzBuzz_if_input_has_contains_3_and_5_and_divisible_by_5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.speak("35")).isEqualTo("FizzBuzzBuzz");
     }
 }
